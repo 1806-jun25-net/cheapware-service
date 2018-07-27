ab9dbf59741e3467dbef8a02b6cf8c6e1d3c35ee
unknown <KyleTTownsend@gmail.com>
2018-07-27 10:31:21 -0400

    wrote ComputerRepo interface

---
+++ Cheapware.Service/Cheapware.Library/RepoClasses/IComputerRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cheapware.Data.Models;
+using Cheapware.Library.Models;
+namespace Cheapware.Library.RepoClasses
+{
+    public interface IComputerRepo
+    {
+        List<ComputerCase> GetComputerCases();
+        ComputerCase GetComputerCaseByName(string name);
+        List<CPU> GetCpus();
+        CPU GetCpuByName(string name);
+        void Save();
+        List<Customer> GetCustomers();
+        void AddCustomer(Customer customer);
+        Customer GetCustomerById(int id);
+        Customer GetCustomerByName(string name);
+        List<GraphicsCard> GetGraphicsCards();
+        GraphicsCard GetGraphicsCardByName(string name);
+        List<Inventory> GetInventory();
+        Inventory GetProductByName(string name);
+        List<MotherBoard> GetMotherBoards();
+        MotherBoard GetMotherBoardByName(string name);
+        List<PowerSupply> GetPowerSupplys();
+        PowerSupply GetPowerSupplyByName(string name);
+        List<RAM> GetRams();
+        RAM GetRamByName(string name);
+    }
+}
