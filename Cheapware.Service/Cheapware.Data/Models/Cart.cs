9a4120cb117c6ce00a4e16e9ef89eadcb35149a6
unknown <KyleTTownsend@gmail.com>
2018-08-01 20:12:55 -0400

    added cart in library

---
+++ Cheapware.Service/Cheapware.Data/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cheapware.Data.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string ProductId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
