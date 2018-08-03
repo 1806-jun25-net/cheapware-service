e02736386ad10d0ee1ac91842b4b8e6a9a6354fe
joeisble <joeisble@yahoo.com>
2018-08-02 20:34:27 -0400

    updated sql files

--- RamAndGraphicsCards.sql
+++ RamAndGraphicsCards.sql
@@ -12,5 +12,6 @@ insert into cheap.Rams values('Team T-Force Vulcan 16GB 2400', '2400 Mhz', '16GB
 
 
 insert into cheap.GraphicsCards values('MSI GeForce GTX 1080 8GB PCI Express 3.0 x16 Video Card', '1632 Mhz', '8GB', 469.99, 'graphicsCard1.jpg');
-insert into cheap.GraphicsCards values('Sapphire Radeon NITRO+ RX 580 8GB PCI-E w/ Backplate SPECIAL EDITION', '1430 Mhz', '8GB', 309.99, 'graphicscard2.jpg');
+insert into cheap.GraphicsCards 
+values('Sapphire Radeon NITRO+ RX 580 8GB PCI-E Backplate SPECIAL EDITION', '1430 Mhz', '8GB', 309.99, 'graphicscard2.jpg');
 insert into cheap.GraphicsCards values('EVGA GeForce GTX 1060 SC GAMING, ACX 2.0 (Single Fan), 3GB', '1607 Mhz', '3GB', 239.99, 'graphicscard3.jpg');
